115168a26c8fcdc4999f0a6dc91c1e2419b3ca34
Opilane TTHK <opilane@tthk.ee>
2023-03-09 14:32:17 +0200

    Mobile Window fix

--- TARge21SciCalculator/TARge21SciCalculator/App.xaml.cs
+++ TARge21SciCalculator/TARge21SciCalculator/App.xaml.cs
@@ -20,8 +20,8 @@ public partial class App : Application
 
 		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
 		{
-			var mauiWindow = Handler.VirtualView;
-			var nativeWindow = Handler.PlatformView;
+			var mauiWindow = handler.VirtualView;
+			var nativeWindow = handler.PlatformView;
 			nativeWindow.Activate();
 			IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
 			WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
