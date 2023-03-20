599a4ba84cb464ab235bb377edd4a8c73ca472a6
klill1 <kert.lillemets@gmail.com>
2023-03-20 22:40:40 +0200

    Updated Version of calculator

--- TARge21SciCalculator/TARge21SciCalculator/Views/CalculatorPage.xaml.cs
+++ TARge21SciCalculator/TARge21SciCalculator/Views/CalculatorPage.xaml.cs
@@ -1,9 +1,16 @@
+using TARge21SciCalculator.ViewModels;
+
 namespace TARge21SciCalculator.Views;
 
 public partial class CalculatorPage : ContentPage
 {
-	public CalculatorPage()
+    private CalculatorPageViewModel _calculatorViewModel;
+
+    public CalculatorPage()
 	{
 		InitializeComponent();
-	}
+
+        _calculatorViewModel = new CalculatorPageViewModel();
+        this.BindingContext = _calculatorViewModel;
+    }
 }
